dd7cf47215c00b7355de54235198a33ade53dc09
Jake Puffer <jjpuffer@sfu.ca>
2020-11-24 20:56:03 -0800

    "Working"

--- Controllers/rectangle.js
+++ Controllers/rectangle.js
@@ -1,8 +1,52 @@
 var nextID = 1;
+var currentIDs = new Map();
 
+//inital fetch from DB
+window.onload = function() {
+    var request = new XMLHttpRequest();
+    request.open("GET", "/initialFetch", true);
+    request.setRequestHeader("Content-type", "application/json;charset=UTF-8");
+    request.onload = function() {
+        //console.log("Inital fetch")
+        //console.log(this.response);
+        var rectsInDB = JSON.parse(this.response);
+        //console.log(rectsInDB.length);
+        //clearTable();
+        for (var i = 0; i < rectsInDB.length; i++) {
+            //console.log(rectsInDB[i].ID);
+            //console.log(parseInt(rectsInDB[i].ID));
+            currentIDs.set(parseInt(rectsInDB[i].ID), '');
+            //console.log(currentIDs);
+            addToTable(rectsInDB[i].ID, rectsInDB[i].Name, rectsInDB[i].Height, rectsInDB[i].Width, rectsInDB[i].Colour, rectsInDB[i].Opacity);
+        }
+        for (var i = 0; i < currentIDs.size; i++) {
+            if (!currentIDs.has(nextID)) {
+                break;
+            }
+            nextID++;
+        }
+        var dispReq = new XMLHttpRequest();
+        dispReq.open("GET", "/displayReq", true);
+        dispReq.setRequestHeader("Content-type", "application/json;charset=UTF-8");
+        dispReq.onload = function() {
+        var option = this.response;
+        //console.log(`DISPLAY OPTION: ${option}`);
+        if (option == "1") {
+        //    console.log("Displaying option 1");
+        //    console.log(currentIDs);
+            displayInOrder();
+        }
+        else if (option == "2") {
+            groupByColour();
+        }
 
+        };
+        dispReq.send();
+    };
+    request.send();
 
-
+    
+};
 
 // adding rect to db
 document.getElementById("SubmitButton").addEventListener("click", function() {
@@ -34,18 +78,56 @@ document.getElementById("SubmitButton").addEventListener("click", function() {
         request.setRequestHeader("Content-type", "application/json;charset=UTF-8");
         request.send(JSON.stringify(params));
         
-        addToTable(-1, name.value, height.value, width.value, colour.value, opacity.value);
-        nextID++;
-        name.value = "";
-        height.value = "";
-        width.value = "";
-        colour.value = "#000000";
-        opacity.value = "50";
+        //addToTable(-1, name.value, height.value, width.value, colour.value, opacity.value);
+        currentIDs.set(nextID, '');
+        for (var i = 0; i < currentIDs.size; i++) {
+            if (!currentIDs.has(nextID)) {
+                break;
+            }
+            nextID++;
+        }
+        location.reload();
+        // name.value = "";
+        // height.value = "";
+        // width.value = "";
+        // colour.value = "#000000";
+        // opacity.value = "100";
     }
 }); 
 
 // fetching rect from db
 document.getElementById("EnterID").addEventListener("click", function() {
+    var id /*, name, height, width, colour, opacity;*/
+    id = document.getElementById("selectID").value;
+    // name = document.getElementById("NameUpdate");
+    // height = document.getElementById("HeightUpdate");
+    // width = document.getElementById("WidthUpdate");
+    // colour = document.getElementById("ColourUpdate");
+    // opacity = document.getElementById("OpacityUpdate");
+    if (!currentIDs.has(parseInt(id))) {
+        window.alert("Pick a rectangle in the database");
+    }
+    else {
+        var request = new XMLHttpRequest();
+        request.open("POST", "/prepResults", true);
+        request.setRequestHeader("Content-type", "application/json;charset=UTF-8");
+        request.onload = function() {
+            console.log("Calling fetchResults")
+            fetchResults();
+        };
+        request.send(JSON.stringify({ID: `${id}`}));
+    }
+    // TODO
+    // send request to server for row matching ID, then populate fields
+    //var fetched;
+
+    
+   // console.log(fetched);
+    
+   
+}); 
+
+function fetchResults() {
     var id, name, height, width, colour, opacity;
     id = document.getElementById("selectID").value;
     name = document.getElementById("NameUpdate");
@@ -53,30 +135,22 @@ document.getElementById("EnterID").addEventListener("click", function() {
     width = document.getElementById("WidthUpdate");
     colour = document.getElementById("ColourUpdate");
     opacity = document.getElementById("OpacityUpdate");
-
-    // TODO
-    // send request to server for row matching ID, then populate fields
-    var fetched;
+    console.log("IN fetchResults");
     var request = new XMLHttpRequest();
-    request.open("POST", "/fetchRect", true);
+    request.open("GET", "/fetchRect", true);
     request.setRequestHeader("Content-type", "application/json;charset=UTF-8");
     request.onload = function() {
-      //  console.log(this.response);
         fetched = JSON.parse(this.response);
-       // console.log(fetched);
-       name.value = fetched.Name;
+        console.log(fetched);
+        name.value = fetched.Name;
         height.value = fetched.Height;
         width.value = fetched.Width;
         colour.value = fetched.Colour;
         opacity.value = fetched.Opacity;
         updateOpac();
     };
-    request.send(JSON.stringify({ID: `${id}`}));
-    
-   // console.log(fetched);
-    
-   
-}); 
+    request.send();
+}
 
 // updating rect in db
 document.getElementById("UpdateButton").addEventListener("click", function() {
@@ -104,31 +178,37 @@ document.getElementById("UpdateButton").addEventListener("click", function() {
             Colour: `${colour.value}`,
             Opacity: `${opacity.value}`
         };
-        deleteRow("UserTable", parseInt(id.value));
-        addToTable(id.value, name.value, height.value, width.value, colour.value, opacity.value);
+        // deleteRow("UserTable", parseInt(id.value));
+        // addToTable(id.value, name.value, height.value, width.value, colour.value, opacity.value);
 
         var request = new XMLHttpRequest();
         request.open("POST", "/updateRect", true);
         request.setRequestHeader("Content-type", "application/json;charset=UTF-8");
         request.send(JSON.stringify(params));
+        location.reload();
     }
     
-    id.value = "";
-    name.value = "";
-    height.value = "";
-    width.value = "";
-    colour.value = "#000000";
-    opacity.value = "50";
+    // id.value = "";
+    // name.value = "";
+    // height.value = "";
+    // width.value = "";
+    // colour.value = "#000000";
+    // opacity.value = "100";
 }); 
 
 // deleting rect from db
 document.getElementById("DeleteButton").addEventListener("click", function() {
     var id, confirmID, name, height, width, colour, opacity;
     id = document.getElementById("selectID");
+
     confirmID = document.getElementById("deleteID");
-    if (confirmID.value != id.value)
+    if (confirmID.value != id.value || id.value == "")
     {
         window.alert("Make sure you input the same ID twice in order to delete a rectangle");
+    }
+    else if (!currentIDs.has(parseInt(id.value)))
+    {
+        window.alert("Make sure the rectangle you want to delete exists");
     }
     else {
         name = document.getElementById("NameUpdate");
@@ -137,11 +217,11 @@ document.getElementById("DeleteButton").addEventListener("click", function() {
         colour = document.getElementById("ColourUpdate");
         opacity = document.getElementById("OpacityUpdate");
 
-        // TODO
+        
         // send ID to server to delete row
         //send to db
         var params = {
-            ID: `${nextID}`,
+            ID: `${id.value}`,
             Name: `${name.value}`,
             Height: `${height.value}`,
             Width: `${width.value}`,
@@ -153,15 +233,17 @@ document.getElementById("DeleteButton").addEventListener("click", function() {
         request.setRequestHeader("Content-type", "application/json;charset=UTF-8");
         request.send(JSON.stringify(params));
 
-        deleteRow("UserTable", parseInt(id.value));
+        location.reload();
+        // deleteRow("UserTable", parseInt(id.value));
+        // currentIDs.delete(parseInt(id.value));
 
-        id.value = "";
-        name.value = "";
-        height.value = "";
-        width.value = "";
-        colour.value = "#000000";
-        opacity.value = "50";
-        confirmID.value = "";
+        // id.value = "";
+        // name.value = "";
+        // height.value = "";
+        // width.value = "";
+        // colour.value = "#000000";
+        // opacity.value = "100";
+        // confirmID.value = "";
     }
 
         
@@ -178,27 +260,36 @@ function addToTable(id, name, height, width, colour, opacity)
     var newColour = newRow.insertCell(4);
     var newOpacity = newRow.insertCell(5);
 
+    newRow.className = "tableRow";
     newID.innerHTML = id > 0 ? id : nextID;
     newName.innerHTML = name;
     newHeight.innerHTML = height;
     newWidth.innerHTML = width;
     newColour.style.backgroundColor = colour;
     newOpacity.innerHTML = opacity + "%";
-    id > 0 ? console.log(`Updated id ${id}`) : console.log("adding");
+    //id > 0 ? console.log(`Updated id ${id}`) : console.log("adding");
 }
+
+// function clearTable () {
+//     var tableRows = document.getElementsByTagName("td");
+//     console.log(tableRows);
+//     for (var i = 0; i < tableRows.length; i++) {
+//         tableRows[i].remove();
+//     }
+// }
 
 function deleteRow(id, givenValue) {
     //var givenValue = document.getElementById("blah").value;
-    console.log(givenValue);
-    console.log(typeof givenValue);
+    //console.log(givenValue);
+   // console.log(typeof givenValue);
     var td = $("#" + id + " td");
     $.each(td, function(i) {
-        console.log(i);
+       // console.log(i);
       if ($(td[i]).text() == givenValue && i%6 === 0) {
         $(td[i]).parent().remove();
       } 
     });
-    console.log("DELETED");
+    //console.log("DELETED");
   }
 
 
@@ -225,11 +316,67 @@ function updateOpac() {
   updateOutput.innerHTML = updateSlider.value + "%";
 }
 
-function inOrder() {
+function wantToDisplay(n) {
+    if (currentIDs.size == 0) {
+        window.alert("Enter a rectangle into the database to display it");
+    }
+    else {
+      //  console.log(`displaying option: ${n}`);
+        var request = new XMLHttpRequest();
+        request.open("POST", "/wantToDisplay", true);
+        request.setRequestHeader("Content-type", "application/json;charset=UTF-8");
+        request.onload = function () {
+      //  console.log("inside req");
+        location.reload();
 
+    };
+    request.send(JSON.stringify({dispOption: n}));
+
+  //  console.log("After request code");
+    }
 }
 
-function groupByColour() {
-    
+
+function displayInOrder() {
+
+    //console.log("displaying in order");
+    //console.log(currentIDs.keys());
+    for (let id of currentIDs.keys()) {
+        var fetched;
+        var request = new XMLHttpRequest();
+        request.open("POST", "/displayRect", true);
+        request.setRequestHeader("Content-type", "application/json;charset=UTF-8");
+       // console.log("displaying");
+        request.onload = function() {
+           // console.log(this.response);
+            // want = (this.response)[i];
+            // console.log(want);
+            fetched = JSON.parse(this.response);
+            //console.log(fetched);
+            var opacity = parseInt(fetched.Opacity)/100;
+            var newRect = document.createElement("div");
+
+            newRect.className = "rectangle";
+           
+            if (fetched.Colour == "#ffffff") {
+                newRect.style = `height:${fetched.Height}px; width:${fetched.Width}px; opacity:${opacity}; background-color:${fetched.Colour}; margin:20px; border: 2px solid black;`;
+            }
+            else {
+                newRect.style = `height:${fetched.Height}px; width:${fetched.Width}px; opacity:${opacity}; background-color:${fetched.Colour}; margin:20px;`;
+            }
+            if (fetched.Colour == "#000000") {
+                newRect.innerHTML = `<p class="rectangle" style="opacity:1; font:bold; color:white"> ${fetched.Name} </p>`;
+            }
+            else {
+                newRect.innerHTML = `<p class="rectangle" style="opacity:1; font:bold; color:black"> ${fetched.Name} </p>`;
+            }
+            
+            document.body.appendChild(newRect);
+            
+
+        };
+        request.send(JSON.stringify({ID: `${id}`}));
+    }
 }
+
 
--- index.js
+++ index.js
@@ -4,52 +4,181 @@ const db = require("mysql");
 
 const app = express();
 
+var rectsInDB = [];
+var IDsInDB = new Map();
+var displayOption = 0;
+
+
+var con = db.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "rectangle"
+})
+
+con.connect((err) => {
+    if (err) throw err;
+    console.log("Connected to db!!");
+})
+
+
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// initial fetch from db
+app.get('/initialFetch', (req, response) => {
+
+    con.query("SELECT * FROM rectangles", (err, res) => {
+        if (err) throw err;
+
+        for (var i = 0; i < res.length; i++) {
+            if (!IDsInDB.has(res[i].ID)) {
+                IDsInDB.set(res[i].ID, '');
+                rectsInDB.push({
+                    ID: res[i].ID,
+                    Name: res[i].Name,
+                    Height: res[i].Height,
+                    Width: res[i].Width,
+                    Colour: res[i].Colour,
+                    Opacity: res[i].Opacity
+                });
+              //  console.log(rectsInDB[i]);
+            }
+        }
+        //waiting = true;
+        response.send(rectsInDB);
+        
+    });
+    
+})
+
+// display request
+app.get('/displayReq', (req, res) => {
+    res.send(displayOption.toString());
+    displayOption = 0;
+})
+
+// wantToDisplay
+app.post('/wantToDisplay', (req, res) => {
+    console.log("reloading");
+    displayOption = req.body.dispOption;
+    res.send("Done");
+})
 
 // adding rect to db
 app.post('/addRect', (req, res) => {
     var atts = req.body;
-    // var query = `INSERT INTO TABLE Rectangles VALUES (${atts.ID}, ${atts.Name}, ${atts.Height}, ${atts.Width}, ${atts.Colour}, ${atts.Opacity})`;
+    var queryString = `INSERT INTO rectangles VALUES (${atts.ID}, "${atts.Name}", ${atts.Height}, ${atts.Width}, "${atts.Colour}", ${atts.Opacity});`;
     //query db
-    console.log("bleh");
+    con.query(queryString, (err, res) => {
+        if (err) throw err;
+        console.log(`Inserted ID: ${req.body.ID}`);
+    })
     console.log(req.body.ID);
+    res.send("Done");
 })
+
+var results = {   
+    ID: "",
+    Name: "",
+    Height: "",
+    Width: "",
+    Colour: "",
+    Opacity: ""
+    };
+
+function getResult(res) {
+    results.ID = res[0].ID;
+    results.Name = res[0].Name;
+    results.Height = res[0].Height;
+    results.Width = res[0].Width;
+    results.Colour = res[0].Colour;
+    results.Opacity = res[0].Opacity;
+    console.log(results);
+}
 
 // fetching rect from db
-app.post('/fetchRect', (req, res) => {
+app.post('/prepResults', (req, res) => {
     var atts = req.body;
-    //var query = `SELECT * FROM Rectangles WHERE ID = ${atts.ID}`;
-    var temp = {   
-    Colour: "#800080",
-    Height: "1",
-    ID: `${req.body.ID}`,
-    Name: "bep",
-    Opacity: "90",
-    Width: "1"};
-
-    res.send(temp);
+    
+    var queryString = `SELECT * FROM rectangles WHERE ID = ${atts.ID};`;
+    con.query(queryString, (err, res) => {
+        if (err) throw err;
+        console.log(`Fetched ID: ${atts.ID}`);
+        //console.log(res[0]);
+        getResult(res);
+    });
+    res.send("fetched");
 })
+
+app.get('/fetchRect', (req, res) => {
+   // console.log("GET FETCH")
+    res.send(results);
+})
+
+app.post('/displayRect', (req, res) => {
+    var atts = req.body;
+    var queryString = `SELECT * FROM rectangles WHERE ID = ${atts.ID};`;
+    con.query(queryString, (err, result) => {
+        if (err) throw err;
+       // console.log(`Displaying ID: ${atts.ID}`);
+        res.send(result[0])
+    });
+})
+
 
 // updating rect in db
 app.post('/updateRect', (req, res) => {
     var atts = req.body;
-    // var query = `UPDATE Rectangles SET Name = ${atts.Name}, Height = ${atts.Height}, Width = ${atts.Width}, Colour = ${atts.Colour}, Opacity = ${atts.Opacity} WHERE ID = ${atts.ID}`;
-    
+    var queryString = `UPDATE rectangles SET Name = "${atts.Name}", Height = ${atts.Height}, Width = ${atts.Width}, Colour = "${atts.Colour}", Opacity = ${atts.Opacity} WHERE ID = ${atts.ID};`;
+    con.query(queryString, (err, res) => {
+        if (err) throw err;
+        for (var i = 0; i < rectsInDB.length; i++) {
+            if (atts.ID == rectsInDB[i].ID) {
+                rectsInDB[i].Name = atts.Name;
+                rectsInDB[i].Height = atts.Height;
+                rectsInDB[i].Width = atts.Width;
+                rectsInDB[i].Colour = atts.Colour;
+                rectsInDB[i].Opacity = atts.Opacity;
+                break;
+            }
+        }
+        //console.log(`Updated ID: ${atts.ID}`);
+    });
 
 })
 
 // deleting rect from db
 app.post('/deleteRect', (req, res) => {
     var atts = req.body;
-    // var query = `DELETE FROM Rectangles WHERE ID = ${atts.ID}`;
-
+   // console.log(atts);
+    var queryString = `DELETE FROM rectangles WHERE ID = ${atts.ID};`;
+    console.log(`DEleteing: ${atts.ID}`)
+    console.log(typeof atts.ID);
+    con.query(queryString, (err, res) => {
+        if (err) throw err;
+        for (var i = 0; i < rectsInDB.length; i++) {
+            if (atts.ID == rectsInDB[i].ID) {
+                for (var j = i; j < rectsInDB.length - 1; j++) {
+                    rectsInDB[j].ID = rectsInDB[j+1].ID
+                    rectsInDB[j].Name = rectsInDB[j+1].Name;
+                    rectsInDB[j].Height = rectsInDB[j+1].Height;
+                    rectsInDB[j].Width = rectsInDB[j+1].Width;
+                    rectsInDB[j].Colour = rectsInDB[j+1].Colour;
+                    rectsInDB[j].Opacity = rectsInDB[j+1].Opacity;
+                }
+                rectsInDB.pop();
+                IDsInDB.delete(parseInt(atts.ID));
+                console.log(rectsInDB);
+                console.log(IDsInDB);
+                break;
+            }
+        }
+       // console.log(`Deleted ID: ${atts.ID}`);
+    });
 })
 
 app.use('/', express.static('.', {index: "Views/rectangle.html"}))
-
-// app.get('/', function(req, res){
-//     res.sendFile('rectangle.html', { root: __dirname + "/Views" } );
-// });
 
 app.listen(3000, () => {console.log("listening to port 3000")});
